c432f14e5b6ac57dab30b7417cf5b026ff95eafc
AliAl-Rady <a2alrady@uwaterloo.ca>
2024-03-17 23:45:59 -0400

    completed week 10 lab

--- src/Components/RestaurantList.js
+++ src/Components/RestaurantList.js
@@ -1,24 +1,24 @@
-import {useEffect} from 'react';
-import {connect} from 'react-redux';
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
 import {loadRestaurants} from '../store/restaurants/actions';
 
-function RestaurantList ({loadRestaurants, restaurants}) {
-  useEffect(() => {
-    loadRestaurants();
-  }, [loadRestaurants]);
+export function RestaurantList({ loadRestaurants, restaurants }) {
+    useEffect(() => {
+        loadRestaurants();
+    }, [loadRestaurants]);
 
-  return (
-    <ul>
-      {restaurants.map(restaurant => (
-        <li key={restaurant.id}>{restaurant.name}</li>
-      ))}
-    </ul>
-  );
-};
+    return (
+        <ul>
+            {restaurants.map(restaurant => (
+                <li key={restaurant.id}>{restaurant.name}</li>
+            ))}
+        </ul>
+    );
+}
 
 const mapStateToProps = state => ({
-  restaurants: state.restaurants.records,
+    restaurants: state.restaurants.records,
 });
-const mapDispatchToProps = {loadRestaurants};
 
+const mapDispatchToProps = {loadRestaurants};
 export default connect(mapStateToProps, mapDispatchToProps)(RestaurantList);
